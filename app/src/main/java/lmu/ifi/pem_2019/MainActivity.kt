b8a1d492fd9e10932093fd119191ff7e00d6a171
Andreas Denk <andreas.denk@maibornwolff.de>
2019-05-27 10:55:00 +0200

    firat changes

---
+++ app/src/main/java/lmu/ifi/pem_2019/MainActivity.kt
@@ -0,0 +1,14 @@
+package lmu.ifi.pem_2019
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        Log.d("log", "Hello World")
+    }
+}
